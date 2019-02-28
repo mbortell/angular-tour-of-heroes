fede0d0acb61dc0139c7071a2f15fe3396b3a8bb
Matt Bortell <mattbortell@Matts-MacBook-Pro.local>
2019-02-28 15:52:04 -0500

    Tutorial Complete

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Buckteeth Boy' },
+    { id: 13, name: 'Fly' },
+    { id: 14, name: 'El Retardo' },
+    { id: 15, name: 'Flatulo' },
+    { id: 16, name: 'Steve' },
+    { id: 17, name: 'Miss Punctuality' },
+    { id: 18, name: 'Brainbox' },
+    { id: 19, name: 'Vibro Man' },
+    { id: 20, name: 'Bug Eater' },
+];
